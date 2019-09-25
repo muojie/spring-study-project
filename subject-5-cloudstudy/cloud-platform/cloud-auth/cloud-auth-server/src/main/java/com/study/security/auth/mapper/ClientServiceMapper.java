5d7a6da4ec08e1411be8fbd373d3bda3d0c26a48
allen <2505507040@qq.com>
2019-09-25 17:19:48 +0800

    delete

--- subject-5-cloudstudy/cloud-platform/cloud-auth/cloud-auth-server/src/main/java/com/study/security/auth/mapper/ClientServiceMapper.java
+++
@@ -1,8 +0,0 @@
-package com.study.security.auth.mapper;
-
-import com.study.security.auth.entity.ClientService;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface ClientServiceMapper extends Mapper<ClientService> {
-    void deleteByServiceId(int id);
-}
